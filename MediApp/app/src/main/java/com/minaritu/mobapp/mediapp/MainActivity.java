a06c8379507f03c4a4ed01dae57d7d43d3027c13
ciemira <chameleon.armie@gmail.com>
2015-09-13 18:14:26 -0700

    BACKUP BEFORE FUCKUP - PRACT APP
    practicing on integrating tabs for mob app

---
+++ MediApp/app/src/main/java/com/minaritu/mobapp/mediapp/MainActivity.java
@@ -0,0 +1,76 @@
+package com.minaritu.mobapp.mediapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.minaritu.mobapp.mediapp.tabs.SlidingTabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager nPager;
+    private SlidingTabLayout nTabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        nPager = (ViewPager)findViewById(R.id.pager);
+        nTabs = (SlidingTabLayout)findViewById(R.id.tabs);
+
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter{
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
+}
